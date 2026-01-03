54d7a65898bf790eb5444565b2afd1a59f4add2e
xed <xedcode69@gmail.com>
2026-01-03 13:04:12 +0530

    fix get-cookie bug

--- src/utils/cookies.util.js
+++ src/utils/cookies.util.js
@@ -3,7 +3,7 @@ export const cookies = {
         res.cookie(cookiename, value, {maxAge: 60000*60, httpOnly: true, sameSite: 'strict'});
     },
     get: (req, cookiename)=>{
-        req.cookies[cookiename];
+        return req.cookies?.[cookiename];
     },
     clear: (res, cookiename)=>{
         res.clearCookie(cookiename);
